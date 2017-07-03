34b3a3d58fee186b93e829cbba3e16910218e046
David Hellmann <davidhellmann.com@gmail.com>
2017-07-03 22:57:48 +0200

    Update Module Creator

--- generators/app/templates/___src/scripts/module.js
+++ generators/app/templates/___src/scripts/module.js
@@ -21,22 +21,28 @@ inquirer
         const jsFile = pkg.src.modules.jsFile
         const cssFile = pkg.src.modules.cssFile
         const templateFile = pkg.src.modules.templateFile
+        const configFile = pkg.src.modules.configFile
 
         try {
-            fs.copyTpl(path.resolve(__dirname, `../scripts/module/${jsFile}`), `${srcPath}/_${answers.moduleName}/${jsFile}`, {
+            fs.copyTpl(path.resolve(__dirname, `../scripts/module/${jsFile}`), `${srcPath}/${answers.moduleName}/${jsFile}`, {
                 moduleName: answers.moduleName,
             })
-            console.log(`${srcPath}/_${answers.moduleName}/${jsFile}`)
+            console.log(`${srcPath}/${answers.moduleName}/${jsFile}`)
 
-            fs.copyTpl(path.resolve(__dirname, `../scripts/module/${cssFile}`), `${srcPath}/_${answers.moduleName}/${cssFile}`, {
+            fs.copyTpl(path.resolve(__dirname, `../scripts/module/${cssFile}`), `${srcPath}/${answers.moduleName}/${cssFile}`, {
                 moduleName: answers.moduleName,
             })
-            console.log(`${srcPath}/_${answers.moduleName}/${cssFile}`)
+            console.log(`${srcPath}/${answers.moduleName}/${cssFile}`)
 
-            fs.copyTpl(path.resolve(__dirname, `../scripts/module/${templateFile}`), `${srcPath}/_${answers.moduleName}/${templateFile}`, {
+            fs.copyTpl(path.resolve(__dirname, `../scripts/module/${templateFile}`), `${srcPath}/${answers.moduleName}/${templateFile}`, {
                 moduleName: answers.moduleName,
             })
-            console.log(`${srcPath}/_${answers.moduleName}/${templateFile}`)
+            console.log(`${srcPath}/${answers.moduleName}/${templateFile}`)
+
+            fs.copyTpl(path.resolve(__dirname, `../scripts/module/${configFile}`), `${srcPath}/${answers.moduleName}/${configFile}`, {
+                moduleName: answers.moduleName,
+            })
+            console.log(`${srcPath}/${answers.moduleName}/${configFile}`)
 
             console.log('Everything created')
             fs.commit(function(done) {
--- generators/module/config/_filesModuleTpl.js
+++ generators/module/config/_filesModuleTpl.js
@@ -9,23 +9,31 @@ const filesModuleTpl = (context, destPath) => {
     if (!context.props.templateFileName.includes('.html')) {
         context.props.templateFileName = context.props.templateFileName + '.html'
     }
+    if (!context.props.configFileName.includes('.json')) {
+        context.props.configFileName = context.props.configFileName + '.json'
+    }
 
     return {
         files: [
             {
-                src: `___src/example/_main.js`,
-                dest: `${destPath}/_${context.props.moduleName}/${context.props.jsFileName}`,
+                src: `___src/example/main.js`,
+                dest: `${destPath}/${context.props.moduleName}/${context.props.jsFileName}`,
                 type: 'js'
             },
             {
-                src: `___src/example/_style.scss`,
-                dest: `${destPath}/_${context.props.moduleName}/${context.props.cssFileName}`,
+                src: `___src/example/style.scss`,
+                dest: `${destPath}/${context.props.moduleName}/${context.props.cssFileName}`,
                 type: 'css'
             },
             {
-                src: `___src/example/_template.html`,
-                dest: `${destPath}/_${context.props.moduleName}/${context.props.templateFileName}`,
+                src: `___src/example/template.html`,
+                dest: `${destPath}/${context.props.moduleName}/${context.props.templateFileName}`,
                 type: 'template'
+            },
+            {
+                src: `___src/example/config.json`,
+                dest: `${destPath}/${context.props.moduleName}/${context.props.templateFileName}`,
+                type: 'config'
             }
         ]
     }
