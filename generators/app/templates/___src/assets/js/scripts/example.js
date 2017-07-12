6187efb94d3a07de9b3e20436439b72044b2825d
David Hellmann <davidhellmann.com@gmail.com>
2017-07-12 20:17:45 +0200

    Branding

--- generators/app/templates/___src/assets/js/scripts/example.js
+++ generators/app/templates/___src/assets/js/scripts/example.js
@@ -7,8 +7,8 @@ console.log(`====================`)
 console.log(`Made with Baukasten`)
 console.log(`Get it here: https://www.npmjs.com/package/generator-baukasten`)
 console.log(`--------------------`)
-console.log(`AUTHOR:  David Hellmann`)
-console.log(`URL:     https://davidhellmann.com`)
+console.log(`Author:  David Hellmann`)
+console.log(`Website: https://davidhellmann.com`)
 console.log(`====================`)
 
 
