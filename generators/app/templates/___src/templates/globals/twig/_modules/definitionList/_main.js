97191e72de7b23e9895d24845cd7e76f7acebbcb
David Hellmann <davidhellmann.com@gmail.com>
2017-07-20 12:20:22 +0200

    Update Modules

---
+++ generators/app/templates/___src/templates/globals/twig/_modules/definitionList/_main.js
@@ -0,0 +1,7 @@
+/* -------------------------------------------------- */
+/*    Definition List
+/* -------------------------------------------------- */
+
+export default () => {
+    console.log('Initialize: definitionList')
+}
