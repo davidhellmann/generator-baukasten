ee7a52e14095a8aa118fe23d1a21f628464fc650
David Hellmann <davidhellmann.com@gmail.com>
2017-10-29 02:23:11 +0200

    Update General for Craft 3 CMS

--- generators/app/templates/___src/_craftCMS3/general.php
+++ generators/app/templates/___src/_craftCMS3/general.php
@@ -1,24 +1,28 @@
 <?php
+
 /**
  * General Configuration
  *
- * All of your system's general configuration settings go in here. You can see a
- * list of the default settings in `vendor/craftcms/cms/src/config/defaults/general.php`.
+ * All of your system's general configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
-// $_ENV constants are loaded by craft3-multi-environment from .env.php via web/index.php
-return [
+// $_ENV constants are loaded by craft-multi-environment from .env.php via public/index.php
+return array(
 
     // All environments
     '*' => [
-
         // Whether "index.php" should be visible in URLs (true, false, "auto")
         'omitScriptNameInUrls' => true,
         'usePathInfo' => true,
         'cacheDuration' => false,
         'generateTransformsBeforePageLoad' => true,
+        'requireMatchingUserAgentForSession' => false,
+        'userSessionDuration' => 'P1W',
+        'rememberedUserSessionDuration' => 'P4W',
         'defaultWeekStartDay' => 1,
         'enableCsrfProtection' => true,
+        'backupDbOnUpdate' => false,
         'cpTrigger' => 'admin',
         'defaultCpLanguage' => 'en',
         'maxUploadFileSize' => 67108864,
@@ -29,6 +33,9 @@ return [
             'subLeft' => true,
             'subRight' => true,
         ],
+
+        'defaultTemplateExtensions' => array('html', 'twig', 'rss'),
+        
         /*
         'siteUrl' => array(
             'de' => getenv('CRAFTENV_SITE_URL') . 'de/',
@@ -38,29 +45,38 @@ return [
         'siteUrl' => getenv('CRAFTENV_SITE_URL'),
         'rootUrl' => getenv('CRAFTENV_SITE_URL'),
         'craftEnv' => CRAFT_ENVIRONMENT,
+
+        // Set the environmental variables
+        'environmentVariables' => array(
+            'baseUrl'  => getenv('CRAFTENV_BASE_URL'),
+            'basePath' => getenv('CRAFTENV_BASE_PATH'),
+            'uploadUrl' => getenv('CRAFTENV_BASE_URL') . 'uploads/',
+            'uploadPath' => getenv('CRAFTENV_BASE_PATH') . 'uploads/',
+        ),
     ],
 
     // Live (production) environment
-    'live' => [
+    'live'  => array(
+        'isSystemOn' => true,
         'devMode' => false,
         'enableTemplateCaching' => true,
         'allowAutoUpdates' => false,
-    ],
+    ),
 
     // Staging (pre-production) environment
-    'staging' => [
+    'staging'  => array(
+        'isSystemOn' => false,
         'devMode' => false,
-        'omitScriptNameInUrls' => false,
         'enableTemplateCaching' => true,
         'allowAutoUpdates' => false,
-    ],
+    ),
 
     // Local (development) environment
-    'local' => [
+    'local'  => array(
+        'isSystemOn' => true,
         'devMode' => true,
-        'omitScriptNameInUrls' => false,
         'enableTemplateCaching' => false,
         'allowAutoUpdates' => true,
-    ]
-
-];
+        'disableDevmodeMinifying' => true,
+    ),
+);
