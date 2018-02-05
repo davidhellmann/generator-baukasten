1111eac07ba5831d3ab1e83283d0ce01188f73a6
davidhellmann <david@hellmann.io>
2018-02-05 16:12:42 +0100

    Update Fontfaceobserver

--- generators/app/templates/___src/assets/js/scripts/fontfaceobserver.js
+++ generators/app/templates/___src/assets/js/scripts/fontfaceobserver.js
@@ -1,17 +1,47 @@
-import FontFaceObserver from 'fontfaceobserver'
+// import FontFaceObserver from 'fontfaceobserver'
 
 // Disable if you use WebFonts
 // document.documentElement.className += " wf-loaded"
 
+
+/**
+ * Check if the User already visited the site and the Fonts were loaded
+ * If not resolve all fonts, otherwise directly set the class to html
+ */
 /*
-const fontA = new FontFaceObserver('fontA')
-const fontB = new FontFaceObserver('fontB')
+const fontA = new FontFaceObserver('fontA');
+const fontB = new FontFaceObserver('fontB');
 
-fontA.load().then(function () {
- document.documentElement.className += " wf-loaded"
- })
-
-Promise.all([fontA.load(), fontB.load()]).then(function () {
-    document.documentElement.className += " wf-loaded"
-});
+if (!sessionStorage.fontsLoaded) {
+    console.log('FontLoaded not in session storage');
+    // Add all Variables defined above in the array of Promise.all
+    Promise.all([fontA.load(), fontB.load()])
+        .then(function() {
+            console.log('Fonts loaded');
+            document.documentElement.classList.add('wf-loaded');
+            sessionStorage.fontsLoaded = true;
+        })
+        .catch((e) => {
+            console.error(e);
+            document.documentElement.classList.add('wf-failed');
+            sessionStorage.fontsLoaded = false;
+        });
+} else {
+    console.log('Fonts loaded via cache');
+    document.documentElement.classList.add('wf-loaded');
+}
 */
+
+
+/**
+ * Single Font Example
+ fontA.load().then(function () {
+    console.log('Fonts loaded');
+    document.documentElement.classList.add('wf-loaded');
+    sessionStorage.fontsLoaded = true;
+}).catch(function () {
+    console.error(e);
+    document.documentElement.classList.add('wf-failed');
+    sessionStorage.fontsLoaded = false;
+});
+ */
