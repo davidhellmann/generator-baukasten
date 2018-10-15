865e2d736c3ae69b740b089386601516c0a710da
David Hellmann <david@hellmann.io>
2018-10-15 23:25:43 +0200

    Update Gulp Tasks

--- generators/app/templates/___src/gulp/tasks/imagesSVGCopy.js
+++ generators/app/templates/___src/gulp/tasks/imagesSVGCopy.js
@@ -29,7 +29,7 @@ const copyVectors = () => {
     .pipe(gulp.dest(pkg.src.images.svg.single + 'inline/'))
     .pipe(gulp.dest(vectorDist + 'inline/'))
     .pipe(gulp.dest(pkg.src.templates + '_partials/svg/'))
-    .pipe(gulp.dest(pkg.dist.markup + '_partials/svg/'));
+    .pipe(gulp.dest(pkg.dist.markup + '_partials/svg/'))
 }
 
 gulp.task('copy:svg-single', copyVectors)
--- generators/app/templates/___src/gulp/tasks/templates.js
+++ generators/app/templates/___src/gulp/tasks/templates.js
@@ -1,16 +1,16 @@
-import gulp from 'gulp'
-import gulpLoadPlugins from 'gulp-load-plugins'
-import pkg from '../../package.json'
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import pkg from '../../package.json';
 
-const $ = gulpLoadPlugins()
+const $ = gulpLoadPlugins();
 
 const templates = () => {
     const inject_folder = <% if (projectType === 'wordpress' ) { %> '_partials/_webpack' <% } else { %> '_partials/webpack' <% } %>
     return gulp
         .src([`!${pkg.src.templates}${inject_folder}/**`, `${pkg.src.templates}**/*.*`])
         .pipe(global.checkChanged === true ? $.changed(pkg.dist.markup) : $.util.noop())
-        .pipe(gulp.dest(pkg.dist.markup))
-}
+        .pipe(gulp.dest(pkg.dist.markup));
+};
 
-gulp.task('compile:templates', templates)
-module.exports = templates
+gulp.task('compile:templates', templates);
+module.exports = templates;
