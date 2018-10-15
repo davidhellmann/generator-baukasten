5e9881c994fd78b2b7dd964252c476a248d7c480
David Hellmann <david@hellmann.io>
2018-10-15 23:14:35 +0200

    Update Plugins SH

--- generators/app/templates/___src/_craftCMS3/plugins.sh
+++ generators/app/templates/___src/_craftCMS3/plugins.sh
@@ -17,134 +17,15 @@ echo
 echo
 echo
 
-echo ''
-echo '>>> Installing: Craft 3 Pathtools'
-composer require nystudio107/craft-pathtools
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Redactor'
-composer require craftcms/redactor
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Craft3 Cookies'
-composer require nystudio107/craft-cookies
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Element API'
-composer require craftcms/element-api
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Craft3 Minify'
-composer require nystudio107/craft-minify
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: SVG Placeholder'
-composer require mblode/svgplaceholder
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Splash'
-composer require ether/splash
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Craft3 Typogrify'
-composer require nystudio107/craft-typogrify
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Sprout Active'
-composer require barrelstrength/sprout-active
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Preparse Field'
-composer require aelvan/preparse-field
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Craft CPCSS'
-composer require doublesecretagency/craft-cpcss
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Expanded Singles'
-composer require verbb/expanded-singles
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Snitch'
-composer require marionnewlevant/snitch
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Craft3 Codemirror'
-composer require luwes/craft3-codemirror
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Craft3 Fields'
-composer require newism/craft3-fields
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Mailgun'
-composer require craftcms/mailgun
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: CP Field Inspect'
-composer require mmikkel/cp-field-inspect
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Craft Schema'
-composer require rias/craft-schema
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Child Me'
-composer require mmikkel/child-me
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Logs'
-composer require ether/logs
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Advanced URL Field'
-composer require charliedev/advanced-url-field
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Craft Dumper'
-composer require studioespresso/craft-dumper
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Supersort'
-composer require topshelfcraft/supersort
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Craft3 Routemap'
-composer require nystudio107/craft-routemap
-echo '--------------------------------------------------'
-
-echo ''
-echo '>>> Installing: Craft3 Image Optimize'
-composer require nystudio107/craft-imageoptimize
-echo '--------------------------------------------------'
 
 echo ''
 echo '>>> Installing: Architect'
 composer require pennebaker/craft-architect
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Asset Usage'
+composer require born05/craft-assetusage
 echo '--------------------------------------------------'
 
 echo ''
@@ -153,33 +34,68 @@ composer require ostark/craft-async-queue
 echo '--------------------------------------------------'
 
 echo ''
-echo '>>> Installing: Command Palette'
-composer require am-impact/amcommand
+echo '>>> Installing: CP Clear Cache'
+composer require mmikkel/cp-clearcache
 echo '--------------------------------------------------'
 
 echo ''
-echo '>>> Installing: Imager'
-composer require aelvan/imager
+echo '>>> Installing: CP Element Count'
+composer require aelvan/craft-cp-element-count
 echo '--------------------------------------------------'
 
 echo ''
-echo '>>> Installing: Seomatic'
-composer require nystudio107/craft-seomatic
+echo '>>> Installing: CP Field Inspect'
+composer require mmikkel/cp-field-inspect
 echo '--------------------------------------------------'
 
 echo ''
-echo '>>> Installing: Super Table'
-composer require verbb/super-table
+echo '>>> Installing: Child Me!'
+composer require mmikkel/child-me
 echo '--------------------------------------------------'
 
 echo ''
-echo '>>> Installing: Blockonomicon'
-composer require charliedev/blockonomicon
+echo '>>> Installing: CodeMirror'
+composer require luwes/craft3-codemirror
 echo '--------------------------------------------------'
 
 echo ''
-echo '>>> Installing: Disqus'
-composer require nystudio107/craft-disqus
+echo '>>> Installing: Control Panel CSS'
+composer require doublesecretagency/craft-cpcss
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Cookies'
+composer require nystudio107/craft-cookies
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Gonzo Patternlib'
+composer require martinherweg/craft-gonzo
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Dumper'
+composer require studioespresso/craft-dumper
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Element API'
+composer require craftcms/element-api
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Environment Label'
+composer require topshelfcraft/environment-label
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Expanded Singles'
+composer require verbb/expanded-singles
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Freeform Lite'
+composer require solspace/craft3-freeform
 echo '--------------------------------------------------'
 
 echo ''
@@ -188,22 +104,77 @@ composer require superbig/craft3-http2serverpush
 echo '--------------------------------------------------'
 
 echo ''
-echo '>>> Installing: Craft Inventory'
-composer require doublesecretagency/craft-inventory
+echo '>>> Installing: ImageOptimize'
+composer require nystudio107/craft-imageoptimize
 echo '--------------------------------------------------'
 
 echo ''
-echo '>>> Installing: Craft Similar'
-composer require nystudio107/craft-similar
+echo '>>> Installing: Image Resizer'
+composer require verbb/image-resizer
 echo '--------------------------------------------------'
 
 echo ''
-echo '>>> Installing: Craft Linkfield'
+echo '>>> Installing: Imager'
+composer require aelvan/imager
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Logs'
+composer require ether/logs
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Mailgun'
+composer require craftcms/mailgun
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Minify'
+composer require nystudio107/craft-minify
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Redactor'
+composer require craftcms/redactor
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Seomatic'
+composer require nystudio107/craft-seomatic
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Splash'
+composer require ether/splash
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Spoon'
+composer require angellco/spoon
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: SuperSort'
+composer require topshelfcraft/supersort
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Super Table'
+composer require verbb/super-table
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Twig Perversion'
+composer require marionnewlevant/twig-perversion
+echo '--------------------------------------------------'
+
+echo ''
+echo '>>> Installing: Typed link field'
 composer require sebastianlenz/linkfield
 echo '--------------------------------------------------'
 
 echo ''
-echo '>>> Installing: Craft Environment Label'
-composer require topshelfcraft/environment-label
+echo '>>> Installing: Typogrify'
+composer require nystudio107/craft-typogrify
 echo '--------------------------------------------------'
 
