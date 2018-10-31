e3bcfd87924d4acc1b84f12bf78ad1e538ac8d4e
davidhellmann <david@hellmann.io>
2018-10-31 15:40:11 +0100

    Update Ferdi
    Signed-off-by: davidhellmann <david@hellmann.io>

--- generators/app/templates/___src/scripts/ferdi/config-template.php
+++ generators/app/templates/___src/scripts/ferdi/config-template.php
@@ -1,9 +1,21 @@
 <?php
-$defaultVariant = [
+// Describe your component
+$componentMeta = [
     'title' => '<%= moduleName.replace('-', '_') %>',
     'status' => 'wip',
+    'visible' => true,
+    'type' => '<%= pathOptions.key.slice(0, -1) %>',
+    'path' => '_<%= pathOptions.key %>/<%= moduleName.replace('-', '_') %>/',
+    'description' => '<%= moduleName.replace('-', '_') %> Description goes here…'
+];
+
+// That's your default Variant. When you need just a single
+// Variant you can describe it all here.
+$defaultVariant = [
+    'title' => '<%= moduleName.replace('-', '_') %>',
+    'status' => '',
     'description' => '<%= moduleName.replace('-', '_') %> Description goes here…',
-    'cn' => 'a-<%= moduleName.replace('-', '_') %>',
+    'cn' => '<%= pathOptions.key.charAt(0) %>-<%= moduleName.replace('-', '_') %>',
     'modifiers' => [],
     'customClasses' => [],
     'data' => [],
@@ -14,20 +26,12 @@ $defaultVariant = [
 ];
 
 return [
-    'meta' => [
-        'title' => '<%= moduleName.replace('-', '_') %>',
-        'status' => 'none',
-        'visible' => true,
-        'type' => '<%= pathOptions.key.slice(0, -1) %>',
-        'path' => '_<%= pathOptions.key %>/<%= moduleName.replace('-', '_') %>/',
-        'description' => '<%= moduleName.replace('-', '_') %> Description goes here…'
-    ],
+    'meta' => array_merge($componentMeta, []),
     'variants' => [
         '<%= moduleName.replace('-', '_') %>' => array_merge($defaultVariant, []),
         /*
-        '<%= moduleName.replace('-', '_') %>--variant' => array_merge($defaultVariant, [
+        '<%= moduleName.replace('-', '_') %>--variantName' => array_merge($defaultVariant, [
             'title' => '<%= moduleName.replace('-', '_') %> Intro',
-            'status' => 'wip',
             'description' => '<%= moduleName.replace('-', '_') %> Description goes here…',
             'modifiers' => ['variant'],
         ]),
