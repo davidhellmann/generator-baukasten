5c38eea7fc518c7fc49623af84debbc511a9eab6
David Hellmann <davidhellmann.com@gmail.com>
2018-11-22 07:55:35 +0100

    Update Gonzo Config

--- generators/app/templates/___src/_craftCMS3/craft-gonzo.php
+++ generators/app/templates/___src/_craftCMS3/craft-gonzo.php
@@ -14,7 +14,7 @@ return [
         "m" => "800px",
         "l" => "1000px",
         "max" => "1440px",
-        "fluid" => "100vw"
+        "fluid" => "100%"
     ],
     "compStatus" => [
         0 => ["name" => "wip", "color" => "#FF9800"],
