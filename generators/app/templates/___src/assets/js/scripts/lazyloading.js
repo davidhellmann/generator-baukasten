4e9e029b6ff2148563ad99fe5a5c59af3a12128b
davidhellmann <david@hellmann.io>
2019-02-12 17:51:33 +0100

    Update Lazyloading
    Signed-off-by: davidhellmann <david@hellmann.io>

--- generators/app/templates/___src/assets/js/scripts/lazyloading.js
+++ generators/app/templates/___src/assets/js/scripts/lazyloading.js
@@ -7,6 +7,11 @@ import 'lazysizes';
 // import '../../../../node_modules/lazysizes/plugins/bgset/ls.bgset'
 import '../../../../node_modules/lazysizes/plugins/respimg/ls.respimg';
 
+document.addEventListener('lazyloaded', (e) => {
+    const target = e.target.parentNode;
+    target.style.backgroundColor = 'transparent';
+});
+
 // Settings
 window.lazySizesConfig = window.lazySizesConfig || {};
 window.lazySizesConfig.expand = 500; // default 360-500
